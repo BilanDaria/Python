433135debb7976076ca34d2a8f6f3fa7298961b9
daria_bilan <daria.bilan02@gmail.com>
2025-06-10 20:54:22 +0300

    coffee machine project

--- 15Day_coffee_machine/main.py
+++ 15Day_coffee_machine/main.py
@@ -10,10 +10,27 @@ def print_report():
     print(f"Money: ${data.money}")
 
 
-def change_resource_amount(coffe_type):
+def print_result(users_change, coffe_type):
+    if users_change < 0:
+        print(f"Here is {users_change * -1} in change.\n"
+              f"Here your {coffe_type}.")
+        change_resource_amount(coffe_type)
+        # data.money += data.MENU[coffe_type]["cost"] + users_change
+    elif users_change == 0:
+        print(f"Here your {coffe_type}.")
+        change_resource_amount(coffe_type)
+        # data.money += data.MENU[coffe_type]["cost"]
+    else:
+        print("Sorry there is not enough money. Money refunded.")
+
+
+def change_resource_amount(coffee_type):
     ingredients = list(data.resources.keys())
     for i in ingredients:
-        data.resources[i] = data.resources.get(i) - data.MENU[coffe_type]["ingredients"][i]
+        if i in data.MENU[coffee_type]["ingredients"]:
+            data.resources[i] = data.resources.get(i) - data.MENU[coffee_type]["ingredients"][i]
+        else:
+            continue
     # print_report()
 
 
@@ -35,41 +52,41 @@ def check_resources(coffee_type):
     # print(needed_resources)
 
 
-def coin_process():
+def coin_process(coffee_cost):
     user_coins_amount = {
         "quarters": 0.00,
         "dimes": 0.00,
         "nickles": 0.00,
         "pennies": 0.00,
     }
-    result = 0
+    result = 0.0
     print("Please insert coins.")
-    for j in user_coins_amount:
-        user_coins_amount[j] = float(input(f"How many {j}?: "))
-        print()
-    # for i in data.COINS_COSTS:
-    #     user_coins_amount[j] =
+    for i, j in zip(user_coins_amount, data.COINS_COSTS):
+        user_coins_amount[i] = float(input(f"How many {i}?: ")) * data.COINS_COSTS[j]
+    result += round(coffee_cost - sum(list(user_coins_amount.values())), 2)
+    if result < 0 or result == 0:
+        data.money += sum(list(user_coins_amount.values())) + result
+    return result
 
 
-
-
-
-# while True:
-#     user_coffee = input("What would you like? (espresso/latte/cappuccino): ").lower()
-#     if user_coffee == 'report':
-#         print_report()
-#     elif user_coffee == 'off':
-#         print("Coffee machine turned off.")
-#         break
-#     elif user_coffee in data.MENU:
-#         enough_resources = check_resources(user_coffee)
-#         if not enough_resources:
-#             break
-#         price = data.MENU[user_coffee]["cost"]
-#         # change =
-#         # print(price)
-#     else:
-#         print("Sorry we don't produce this type of drinks. Please, choose from available menu 😉")
+while True:
+    user_coffee = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_coffee == 'report':
+        print_report()
+    elif user_coffee == 'off':
+        print("Coffee machine turned off.")
+        break
+    elif user_coffee in data.MENU:
+        enough_resources = check_resources(user_coffee)
+        if not enough_resources:
+            break
+        price = data.MENU[user_coffee]["cost"]
+        print(f"Price is: {price}")         # Test print
+        change = coin_process(price)
+        print(f"Change will be: {change}")  # Test print
+        print_result(change, user_coffee)
+    else:
+        print("Sorry we don't produce this type of drinks. Please, choose from available menu 😉")
 
 
 
@@ -81,4 +98,4 @@ def coin_process():
 # print("-----------------")
 # change_resource_amount("latte")
 # print(data.COINS_COSTS.get("PENNY"))
-coin_process()
+# coin_process()
