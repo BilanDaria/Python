d09ad3ecd4242a142389cd644927b5d25c89b0f9
daria_bilan <daria.bilan02@gmail.com>
2025-06-21 20:05:44 +0300

    Easy. Calculator

---
+++ 17Day_quiz_game/additional_practice/easy_calculator/Calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+
+    def add(self):
+        return self.number1 + self.number2
+
+    def subtract(self):
+        return self.number1 - self.number2
+
+    def multiply(self):
+        return self.number1 * self.number2
+
+    def divide(self):
+        if self.number2 == 0:
+            return "You cannot divide by zero!"
+        else:
+            return self.number1 / self.number2
