ccb45dac61bc7ef24bf3dae1921bbc31b2eb3ecd
daria_bilan <daria.bilan02@gmail.com>
2025-06-21 20:55:10 +0300

    student grader + refactor book manager and calculator

--- 17Day_quiz_game/additional_practice/easy_calculator/main.py
+++ 17Day_quiz_game/additional_practice/easy_calculator/main.py
@@ -5,24 +5,24 @@ print("Possible math operation:\n"
       "subtract: -,\n"
       "multiply: *,\n"
       "divide: /")
-num1 = float(input("Number 1: "))
-operation = input("Operation: ")
-num2 = float(input("Number 2: "))
+
+expression = input("Enter expression (e.g., 5 * 2): ")
+
+num1_str, operator, num2_str = expression.split()
+num1 = float(num1_str)
+num2 = float(num2_str)
 calc = Calculator(num1, num2)
 
+operations = {
+    "+": calc.add(),
+    "-": calc.subtract(),
+    "*": calc.multiply(),
+    "/": calc.divide(),
+}
 
-if operation == "+":
-    result = calc.add()
-    print(f"{num1} + {num2} = {result}")
-elif operation == "-":
-    result = calc.subtract()
-    print(f"{num1} - {num2} = {result}")
-elif operation == "*":
-    result = calc.multiply()
-    print(f"{num1} * {num2} = {result}")
-elif operation == "/":
-    result = calc.divide()
-    print(f"{num1} / {num2} = {result}")
+if operator in operations:
+    result = operations[operator]
+    print(f"{num1} {operator} {num2} = {result}")
 else:
     print("Unsupported math operation!")
 
