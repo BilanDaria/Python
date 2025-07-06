19c11aa6bab3a9efb70171ebb53da82f99b41614
daria_bilan <daria.bilan02@gmail.com>
2025-07-06 21:44:22 +0300

    Hard. Task Manager. Main logic with functions

--- 17Day_quiz_game/additional_practice/hard_task_manager_with_deadlines_and_priorities/main.py
+++ 17Day_quiz_game/additional_practice/hard_task_manager_with_deadlines_and_priorities/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from pathlib import Path
 
 from task_manager import TaskManager
 import data
@@ -6,6 +7,19 @@ import data
 task_manager = TaskManager()
 all_tasks = []
 overdue_tasks = []
+sorted_tasks = []
+
+path_for_all_tasks = Path("D:\\Personal\\Programming\\Course Hunter\\Python\\100days\\17Day_quiz_game\\additional_practice\\hard_task_manager_with_deadlines_and_priorities\\all.txt")
+path_for_overdue_tasks = Path("D:\\Personal\\Programming\\Course Hunter\\Python\\100days\\17Day_quiz_game\\additional_practice\\hard_task_manager_with_deadlines_and_priorities\\overdue.txt")
+path_for_last_sorted_tasks = Path("D:\\Personal\\Programming\\Course Hunter\\Python\\100days\\17Day_quiz_game\\additional_practice\\hard_task_manager_with_deadlines_and_priorities\\sorted.txt")
+
+
+def get_id(prompt='Enter task id: '):
+    try:
+        return int(input(prompt))
+    except ValueError:
+        print("Invalid input.\n ")
+        return get_id()
 
 
 def get_title(prompt='Enter title: '):
@@ -13,7 +27,7 @@ def get_title(prompt='Enter title: '):
         return input(prompt)
     except ValueError:
         print("Invalid input.\n ")
-        get_title()
+        return get_title()
 
 
 def get_description(prompt='Enter description: '):
@@ -21,7 +35,7 @@ def get_description(prompt='Enter description: '):
         return input(prompt)
     except ValueError:
         print("Invalid input.\n ")
-        get_description()
+        return get_description()
 
 
 def get_deadline(prompt='Enter deadline in yy-mm-dd: '):
@@ -31,7 +45,7 @@ def get_deadline(prompt='Enter deadline in yy-mm-dd: '):
         return deadline
     except ValueError:
         print("Invalid input.\n ")
-        get_deadline()
+        return get_deadline()
 
 
 def get_priority(prompt='Enter priority (Low, Medium, High, Critical): '):
@@ -40,19 +54,195 @@ def get_priority(prompt='Enter priority (Low, Medium, High, Critical): '):
         return data.Priority[priority].name
     except KeyError:
         print("Invalid priority type.\n")
-        get_priority()
+        return get_priority()
 
 
-def get_status(prompt='Enter status (Open, In_progress, Blocked, Done, Rejected): ')
+def get_status(prompt='Enter status (Open, In_progress, Blocked, Done, Rejected): '):
     status = input(prompt).upper()
     try:
         return data.Status[status].name
     except KeyError:
         print("Invalid priority type.\n")
-        get_status()
+        return get_status()
+
+
+def get_all_attributes():
+    title = get_title()
+    desc = get_description()
+    deadline = get_deadline()
+    priority = get_priority()
+    return title, desc, deadline, priority
+
+
+def add_new_task():
+    print("Create new task:")
+    # title = get_title()
+    # desc = get_description()
+    # deadline = get_deadline()
+    # priority = get_priority()
+    title, desc, deadline, priority = get_all_attributes()
+    new_id = task_manager.add_task(title, desc, deadline, priority)
+    print("New task successfully added:\n"
+          f"{task_manager.find_task(new_id)}")
+
+
+def find_the_task():
+    task = task_manager.find_task(get_id())
+    if not task:
+        print("Task not found")
+        return
+    print(task)
+
+
+param_to_change = {
+    "title": get_title,
+    "desc": get_description,
+    "deadline": get_deadline,
+    "priority": get_priority,
+    "status": get_status,
+}
+
+prompt_to_change_task = ("What would you like to change?"
+                         "(title, description, deadline, priority, status, exit(to cancel editing))\n"
+                         "Enter your choice: ")
+
+
+def change_existing_task(task_id):
+    print(f"Let's change task {task_id}: ")
+    try:
+        param = input(prompt_to_change_task)
+    except ValueError:
+        print("Invalid input.")
+        return
+    if param == 'exit':
+        print("Editing canceled.")
+        return
+    if param not in param_to_change:
+        print(f"You can't change {param}")
+        change_existing_task(task_id)
+    new_data = param_to_change[param]()
+    edited_task = task_manager.change_task(task_id, param, new_data)
+    if not edited_task:
+        print(f"Error editing task {task_id}. Recheck your data and try again.")
+        return
+    print(edited_task)
+
+
+def sort_tasks():
+    if len(all_tasks) == 0 and len(overdue_tasks) == 0:
+        print("You don't have any tasks. Try to get all tasks before sort them.")
+        return
+    if len(all_tasks) == 0:
+        tasks = overdue_tasks
+    elif len(overdue_tasks) == 0:
+        tasks = all_tasks
+    else:
+        ans = input("You'd like to sort all task or overdue? Enter a - for all, o - for overdue: ").lower()
+        if ans == 'a':
+            tasks = all_tasks
+        elif ans == 'o':
+            tasks = overdue_tasks
+        else:
+            print("Unknown list of tasks.")
+            return
+    order = input("Enter the order. T - for descending, F - for ascending")
+    result = task_manager.sort_by_deadlines(order, tasks)
+    return result
+
+
+def save_tasks():
+    global all_tasks
+    global overdue_tasks
+    global sorted_tasks
+
+    ans = input("Which tasks you'd like to save? Enter a - for all, o - for overdue, s - for last sorted.: ").lower()
+    if ans == "a":
+        all_tasks.clear()
+        all_tasks = task_manager.get_all_tasks()
+        task_manager.save_to_json(path_for_all_tasks, all_tasks)
+    elif ans == "o":
+        overdue_tasks.clear()
+        overdue_tasks = task_manager.get_overdue_task()
+        task_manager.save_to_json(path_for_overdue_tasks, overdue_tasks)
+    elif ans == "s":
+        task_manager.save_to_json(path_for_last_sorted_tasks, sorted_tasks)
+
+
+def first_task():
+    print("Let's start with the first task in our workspace!")
+    ans = input("Would you like to create your first task? y/n: ").lower()
+    if ans == 'n':
+        print('What a pitiful situation... What else you can do in Task Manager program without tasks 🤔.')
+        return
+    add_new_task()
+
+
+def main_block(point):
+    global all_tasks
+    global overdue_tasks
+    global sorted_tasks
+
+    if point == 1:
+        add_new_task()
+    elif point == 2:
+        find_the_task()
+    # elif point == 3:
+    #     task_id = get_id()
+    #     change_existing_task(task_id)
+    elif point == 4:
+        task_id = get_id()
+        task_manager.delete_task(task_id)
+        print(f"Task {task_id} successfully deleted.")
+    elif point == 5:
+        all_tasks.clear()
+        all_task = task_manager.get_all_tasks()
+        print(task_manager)
+    # elif point == 6:
+    #     overdue_tasks.clear()
+    #     overdue_tasks = task_manager.get_overdue_task()
+    #     print(overdue_tasks)
+    # elif point == 7:
+    #     sorted_tasks.clear()
+    #     sorted_tasks = sort_tasks()
+    #     print(sorted_tasks)
+    # else:
+    #     save_tasks()
+
+
+menu_items = ("What you may to do (enter the number of chose option):\n"
+              "0 - escape? 😈\n"
+              "1 - create new task\n"
+              "2 - find exact task\n"
+              "3 - change existing task\n"
+              "4 - delete task (are you sure?)\n"
+              "5 - get all tasks\n"
+              "6 - sort tasks by deadlines\n"
+              "7 - get all overdue tasks\n"
+              "8 - save tasks to file\n"
+              "Your choice: ")
 
 
 def start():
-    pass
+    global all_tasks
+    all_tasks = task_manager.get_all_tasks()
+    if len(all_tasks) == 0:
+        first_task()
+
+    while True:
+        try:
+            point = int(input(menu_items))
+        except TypeError:
+            print("Enter a number of prefer option)")
+            continue
+        if point == 0:
+            print("Bye, sunshine) See u later 😘")
+            break
+        if 0 < point >= 8:
+            print("...\nTry again, sweety)")
+            return
+        main_block(point)
+
 
 start()
+# all_tasks = task_manager.get_all_tasks()
+# print(type(all_tasks))
--- 17Day_quiz_game/additional_practice/hard_task_manager_with_deadlines_and_priorities/task_manager.py
+++ 17Day_quiz_game/additional_practice/hard_task_manager_with_deadlines_and_priorities/task_manager.py
@@ -1,12 +1,10 @@
 from datetime import datetime
-from pathlib import Path
 import json
 
-from data import IDCounter, change_point
+from data import IDCounter, change_point, data
 from task import Task
 
 n_id = IDCounter()
-path = Path("D:\\Personal\\Programming\\Course Hunter\\Python\\100days\\17Day_quiz_game\\additional_practice\\medium_bank_account")
 
 
 class TaskManager:
@@ -14,12 +12,16 @@ class TaskManager:
     def __init__(self):
         self.list_of_tasks = []
 
-    def add_task(self, title, description, deadline: datetime, priority):
-        new_task = Task(n_id, title, description, deadline, priority)
+
+    def add_task(self, title, description, deadline, priority):
+        new_task = Task(n_id.get_next_id(), title, description, deadline, priority)
         self.list_of_tasks.append(new_task)
+        return self.list_of_tasks[-1].id
 
     def find_task(self, task_id):
-        return next((i for i in self.list_of_tasks if task_id == i.id), None)
+        # task = next((i for i in self.list_of_tasks if i.id == task_id), None)
+        task = next((i for i in self.list_of_tasks if task_id == i.id), None)
+        return task
 
     def change_task(self, task_id, point, new_data):
         if point not in change_point:
@@ -45,22 +47,28 @@ class TaskManager:
         return f"Task {task_id} was successfully deleted."
 
     @staticmethod
-    def sort_by_deadlines(self, order, tasks):
-        sorted_task = sorted(tasks, key=lambda x: x.deadline, reverse=(order if order else False))
+    def sort_by_deadlines(order, tasks_list):
+        sorted_task = sorted(tasks_list, key=lambda x: x.deadline, reverse=order)
         return sorted_task
 
     def get_all_tasks(self):
-        return self.list_of_tasks
+        return list(self.list_of_tasks)
 
     def get_overdue_task(self):
         current_date = datetime.date()
         overdue_tasks = [i for i in self.list_of_tasks if current_date > i.deadline]
         return overdue_tasks
 
-    def save_to_json(self):
-        path.write_text(json.dumps(self.list_of_tasks, indent=4))
+    @staticmethod
+    def save_to_json(path, tasks_list):
+        path.write_text(json.dumps(tasks_list, indent=4))
 
     def __str__(self):
         return '\n'.join(str(i) for i in self.list_of_tasks)
 
 
+# t = TaskManager()
+#
+# print(t.add_task("jhdfvj", "jkdfv", 2025-8-8, "Low"))
+# print(t)
+# print(t.find_task(1))
