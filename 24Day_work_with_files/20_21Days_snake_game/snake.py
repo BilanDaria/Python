eea77bb0a228432b6744abcf14624bd3463e89ee
daria_bilan <daria.bilan02@gmail.com>
2025-08-23 15:54:38 +0300

    work with files

---
+++ 24Day_work_with_files/20_21Days_snake_game/snake.py
@@ -0,0 +1,60 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+        for i in STARTING_POSITIONS:
+            self.add_segment(i)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.up()
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.snake_body.append(new_segment)
+
+    def reset_snake(self):
+        for i in self.snake_body:
+            i.goto(1000, 1000)
+        self.snake_body.clear()
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def extend(self):
+        self.add_segment(self.snake_body[-1].position())
+
+    def move(self):
+        for i in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[i - 1].xcor()
+            new_y = self.snake_body[i - 1].ycor()
+            self.snake_body[i].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
