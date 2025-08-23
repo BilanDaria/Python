a39a9b7106aed584b5f4fa9cdeb6a43f25297cc8
daria_bilan <daria.bilan02@gmail.com>
2025-08-23 14:29:07 +0300

    Full turtle crossing game

--- 23Day_turtle_cross_the_road/blocks.py
+++ 23Day_turtle_cross_the_road/blocks.py
@@ -16,18 +16,34 @@ class Block(Turtle):
         self.shapesize(2, 1)
         self.penup()
         self.color(choice(BLOCK_COLORS))
-        self.goto(START_X_POSITION, self.get_to_new_position())
-        self.move_speed = 0.1
+        # self.goto(280, self.get_to_new_position())
+        # self.goto(randint(-280, 280), self.get_to_new_position())
+        self.goto(self.get_start_x_position(), self.get_y_position())
+        self.move_speed = 0.5
+        self.x_step = 10
 
-    def get_to_new_position(self):
+    @staticmethod
+    def get_start_x_position():
+        x_position = randint(-280, 280)
+        return x_position
+
+    def get_y_position(self):
         global last_y_position
-        new_y_position = randint(-280, 280)
+        y_position = randint(-260, 280)
 
-        if last_y_position == new_y_position:
-            return self.get_to_new_position()
+        if last_y_position == y_position:
+            return self.get_y_position()
 
-        last_y_position = new_y_position
-        return new_y_position
+        last_y_position = y_position
+        return y_position
+
+    def go_to_new_position(self):
+        self.goto(START_X_POSITION, self.get_y_position())
 
     def move(self):
-        self.goto(self.xcor() + self.move_speed, )
+        if self.xcor() <= -300:
+            self.go_to_new_position()
+        self.goto(self.xcor() - (self.x_step * self.move_speed), self.ycor())
+
+    def increase_speed(self):
+        self.move_speed += 0.3
--- 23Day_turtle_cross_the_road/main.py
+++ 23Day_turtle_cross_the_road/main.py
@@ -2,14 +2,22 @@ from time import sleep
 from turtle import Screen
 from turtle_tommy import Tommy
 from blocks import Block
+from announcement import Announcement
 
 screen = Screen()
 screen.setup(600, 600)
+screen.title("Turtle crossing game")
 screen.tracer(0)
 
-block = Block()
-
+announcement = Announcement()
 tommy = Tommy()
+
+blocks_list = []
+blocks_amount = 10
+for i in range(blocks_amount):
+    block = Block()
+    blocks_list.append(block)
+
 
 screen.listen()
 screen.onkeypress(tommy.move_up, "w")
@@ -22,7 +30,21 @@ while is_game_on:
     screen.update()
     sleep(0.1)
 
+    for i in blocks_list:
+        # Detect turtle collision with the block
+        if tommy.distance(i) <= 20:
+            is_game_on = False
+            announcement.game_over()
+            break
+        i.move()
 
+    # Detect turtle collision with upper edge
+    if tommy.ycor() >= 295:
+        announcement.new_level()
+        tommy.start_position()
+        blocks_amount += 5
+        for i in blocks_list:
+            i.increase_speed()
 
 
 screen.exitonclick()
